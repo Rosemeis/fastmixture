2aec4f4f47d29c9d6a944855a7b94b1a0f3d4f44
Rosemeis <meisnerucph@gmail.com>
2023-07-16 13:55:17 +0200

    testing

--- src/main.py
+++ src/main.py
@@ -173,6 +173,8 @@ def main():
 				Bs = np.sort(b)
 				functions.squaremBatch(G, P, Q, a, sumP1, sumP2, sumQA, sumQB, \
 					diffP1, diffP2, diffP3, diffQ1, diffQ2, diffQ3, Bs, args.threads)
+				em_batch.updateP(G, P, Q, sumQA, sumQB, a, Bs, args.threads)
+				em.updateQ(Q, sumQA, sumQB, a)
 		else: # SQUAREM full updates
 			functions.squarem(G, P, Q, a, sumP1, sumP2, sumQA, sumQB, \
 				diffP1, diffP2, diffP3, diffQ1, diffQ2, diffQ3, args.threads)
