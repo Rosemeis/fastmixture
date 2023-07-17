98c51b1ed32983cd2aeb1de596739cd52731a14a
Jonas Meisner <meisnerucph@gmail.com>
2023-07-17 14:39:12 +0200

    yay

--- scripts/loglike.py
+++ scripts/loglike.py
@@ -20,6 +20,8 @@ parser.add_argument("-t", "--threads", type=int, default=1,
 	help="Number of threads")
 parser.add_argument("--bound", type=float, default=1e-5,
 	help="Edge bound for 0 and 1")
+parser.add_argument("--scope", action="store_true",
+	help="Indicator for SCOPE output files")
 
 # Check input
 args = parser.parse_args()
@@ -40,7 +42,6 @@ from src import em
 from src import functions
 
 ### Read data
-print("Reading data...", end="\r")
 # Finding length of .fam and .bim file and finding chromosome indices
 N = functions.extract_length(f"{args.bfile}.fam")
 M = functions.extract_length(f"{args.bfile}.bim")
@@ -50,7 +51,6 @@ with open(f"{args.bfile}.bed", "rb") as bed:
 	G = np.fromfile(bed, dtype=np.uint8, offset=3)
 B = ceil(N/4) # Length of bytes to describe n individuals
 G.shape = (M, B)
-print(f"Loaded {N} samples and {M} SNPs.")
 
 ### Initalize parameters
 f = np.zeros(M)
@@ -58,9 +58,11 @@ lkVec = np.zeros(M)
 
 # Load Q and P file
 Q = np.loadtxt(f"{args.qfile}", dtype=float)
-if Q.shape[0] < Q.shape[1]:
+if args.scope:
 	Q = np.ascontiguousarray(Q.T)
 P = np.loadtxt(f"{args.pfile}", dtype=float)
+if args.scope:
+	P = 1 - P
 assert Q.shape[0] == N, "Number of individuals doesn't match!"
 assert P.shape[0] == M, "Number of SNPs doesn't match!"
 
